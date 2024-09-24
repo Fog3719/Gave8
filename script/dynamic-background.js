d23fa9c15bcf6bca5b23d57eff8fbb481dc720c2
txx <tangxinxu88@gmail.com>
2024-09-24 13:48:45 +0800

    更新目录结构

--- script/dynamic-background.js
+++
@@ -1,8 +0,0 @@
-
-import { AmbientLightBg } from "../jsm/AmbientLightBg.module.js"
-
-let colorbg = new AmbientLightBg({
-	dom: "box",
-	colors: ["#4A86B4","#265999","#1781C2","#0CBCEB","#caa4ea","#4A86B4"],
-	loop: true
-})
